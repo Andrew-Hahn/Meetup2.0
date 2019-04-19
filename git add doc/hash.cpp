265ee05b21ecae8d72fee71c88b0b62a48dfceac
robert961 <49416832+robert961@users.noreply.github.com>
2019-04-19 10:44:42 -0400

    Create hash.cpp
    cpp file for my creating hash table functions

---
+++ git add doc/hash.cpp
@@ -0,0 +1,109 @@
+#include <string>
+#include <iostream>
+#include <list>
+#include <fstream>
+#include <iomanip>
+#include "hash.hpp"
+
+// node* HashTable::createNode(string key)
+// {
+//     node* nw = new node;
+//     nw->key = key;
+//     nw->next = nullptr;
+//     return nw;
+// }
+
+HashTable::HashTable(int bsize)
+{
+    this->tableSize= bsize;
+    hashTable = new node*[tableSize];
+    for(int i=0;i<bsize;i++)
+        hashTable[i] = nullptr;
+}
+
+unsigned int HashTable::hashFunction(int key)
+{
+  //cout<<key<<endl;
+  return (key % tableSize);
+    // unsigned int hashValue = 5381;
+    // int length = key.length();
+    // for (int i=0;i<length;i++)
+    // {
+    //   hashValue=((hashValue<<5)+hashValue) + key[i];
+    // }
+    // hashValue %= tableSize;
+    // return hashValue;
+
+}
+
+// node* HashTable::searchItem(string key)
+// {
+//     //Compute the index by using the hash function
+//     int a=0;
+//     int index = hashFunction(stoi(key[0]);
+//     node* temp=hashTable[index];
+//     while(temp!=nullptr){
+//       if(temp->key==key){
+//         return temp;
+//       }
+//       else{
+//         if(temp->next==nullptr){
+//           return nullptr;
+//         }
+//         temp=temp->next;
+//       }
+//     }
+//     return nullptr;
+//
+//     //TODO: Search the list at that specific index and return the node if found
+// }
+
+bool HashTable::insertItem(string key)
+{
+
+        // TODO :
+        // Use the hash function on the key to get the index/slot,
+        // create a new node with the key and insert it in this slot's list
+        //cout<<key[0]<<endl;
+        int a=key[0]-'0';
+        int index = hashFunction(a);
+        node* newnode= new node;
+        node* temp= hashTable[index];
+        node* prev=nullptr;
+        newnode->key=key;
+        newnode->next=nullptr;
+        if(temp==nullptr){
+          hashTable[index]=newnode;
+          return true;
+        }
+        while(temp!=nullptr){
+          prev=temp;
+          temp=temp->next;
+        }
+        prev->next=newnode;
+        return true;
+
+
+
+}
+
+void HashTable::printTable()
+{
+    for (int i = 0; i < tableSize; i++) {
+        cout << i <<"|| ";
+        node* temp=hashTable[i];
+        while(temp!=nullptr){
+          cout<<temp->key<<" ";
+          temp=temp->next;
+
+        }
+          cout<<endl;
+
+        //TODO
+    }
+
+ }
+
+// bool HashTable::findThreeSum(int arr[], HashTable &ht){
+
+// }
